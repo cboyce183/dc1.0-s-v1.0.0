56e310afacd1cb2fbc69b6cdc0c7dae6938f6ae6
MPastorMeseguer <m.pastormeseguer@gmail.com>
2017-11-18 00:55:43 +0100

    Implement socket method for receiving and sending data in backend

--- server/index.js
+++ server/index.js
@@ -2,20 +2,24 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
   console.log('A user connected!');
 
-  socket.on('disconnect', function(){
+  socket.on('disconnect', function() {
     console.log('user disconnected');
   });
 
-  socket.on('error', function(){
+  socket.on('error', function() {
     console.log('An error has occured');
   });
+
+  socket.on('send', function(data) {
+    socket.emit('receive', data)
+  })
 });
 
 
