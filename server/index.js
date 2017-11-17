54a8931f83f95f7135c354089d5a147f8f7a2b96
vidocco <jack.mk.thibaut@gmail.com>
2017-11-17 20:20:25 +0100

    server bones implemented. app bones implemented.

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('A user connected!');
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+  socket.on('error', function(){
+    console.log('An error has occured');
+  });
+});
+
+
+http.listen(process.env.PORT || 3000, function(){
+  console.log('listening on port:3000');
+});
