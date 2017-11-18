a708a9233d1a46cb92e6030e1dfe9afd2203dccb
vidocco <jack.mk.thibaut@gmail.com>
2017-11-18 16:55:36 +0100

    feat: text editor working on frontend

--- uncode/src/Text.js
+++ uncode/src/Text.js
@@ -9,6 +9,41 @@ class Text extends Component {
     this.lines = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50]
   }
 
+  convertKeys ( event ) {
+    console.log(this.text.selectionStart)
+    if (event.key === 'Tab') {
+      event.preventDefault();
+      const start = this.text.selectionStart;
+      const end = this.text.selectionEnd;
+      this.text.value = this.text.value.slice(0, start) + '  ' + this.text.value.slice(start);
+      this.text.selectionStart = start + 2;
+      this.text.selectionEnd = end + 2;
+    } else if (event.key === '{') {
+      event.preventDefault();
+      const start = this.text.selectionStart;
+      const end = this.text.selectionEnd;
+      this.text.value = `${this.text.value.slice(0, start)}{
+
+}${this.text.value.slice(start)}`;
+      this.text.selectionStart = start + 2;
+      this.text.selectionEnd = end + 2;
+    } else if (event.key === '[') {
+      event.preventDefault();
+      const start = this.text.selectionStart;
+      const end = this.text.selectionEnd;
+      this.text.value = this.text.value.slice(0, start) + '[]' + this.text.value.slice(start);
+      this.text.selectionStart = start + 1;
+      this.text.selectionEnd = end + 1;
+    } else if (event.key === '(') {
+      event.preventDefault();
+      const start = this.text.selectionStart;
+      const end = this.text.selectionEnd;
+      this.text.value = this.text.value.slice(0, start) + '()' + this.text.value.slice(start);
+      this.text.selectionStart = start + 1;
+      this.text.selectionEnd = end + 1;
+    }
+  }
+
   //=============================================== REDERING
 
   renderLineNumbers () {
@@ -23,6 +58,7 @@ class Text extends Component {
         <textarea
           ref={el => this.text = el}
           onChange={() => this.props.func(this.text.value)}
+          onKeyDown={(e) => this.convertKeys(e)}
           className="Text"
           placeholder={this.props.placeholder}/>
       </div>
