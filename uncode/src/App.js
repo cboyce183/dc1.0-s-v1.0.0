714a08381d0bcc0e2d34c7435990b36098386b9f
Javier Moran Lemus <moranlemusj23@gmail.com>
2017-11-18 17:54:27 +0100

    drag and drop, no styling what so ever

--- uncode/src/App.js
+++ uncode/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 import SocketIoClient from 'socket.io-client';
 import ReactFileReader from 'react-file-reader';
+import  Dropzone from  'react-dropzone';
 
 class App extends Component {
 
@@ -28,21 +29,26 @@ class App extends Component {
   }
 
   handleFileChange = files => {
-    var reader = new FileReader();
+    try { var reader = new FileReader();
     reader.onload = () => {
       this.inputText.value = reader.result;
       console.log(reader.result)
     }
     console.log(reader.readAsText(files[0]));
+  } catch (e) {
+    alert('Invalid file type, please upload a JavaScript file');
+  }
   }
 
   render() {
     return (
       <div className="App">
-        <ReactFileReader handleFiles={this.handleFileChange} 
-                         fileTypes={'.js'}>
-          <button className='btn'>Upload</button>
-        </ReactFileReader>
+      <Dropzone onDrop={this.handleFileChange} accept='.js'>
+          <ReactFileReader handleFiles={this.handleFileChange} 
+                          fileTypes={'.js'}>
+            <button className='btn'>Upload</button>
+          </ReactFileReader>
+        </Dropzone>
         <nav className="Header">
           <div className="MaxWidth">
             <h1>UNCODE</h1>
@@ -51,11 +57,11 @@ class App extends Component {
         <div>
           <div className="MaxWidth">
             <div className="Form">
-              <input
-                ref={el => this.inputText = el}
-                type="text"
-                onChange={this.handleTextChange}
-                className="Text"/>
+                <input
+                  ref={el => this.inputText = el}
+                  type="text"
+                  onChange={this.handleTextChange}
+                  className="Text"/>
               <div className="Separator"></div>
               <input className="Text" value={this.state.outputText} placeholder="OUTPUT GOES HERE"/>
             </div>
