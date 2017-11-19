adf02376588cb189ddc0c4ebfe3194aaf8db8cac
vidocco <jack.mk.thibaut@gmail.com>
2017-11-19 02:45:08 +0100

    refactor: About component + about re-styling

--- uncode/src/App.js
+++ uncode/src/App.js
@@ -7,11 +7,10 @@ import { LogoAnim } from './logo/logo-anim';
 import SocketIoClient from 'socket.io-client';
 
 import Text from './Text';
+import About from './About';
 
 import './App.css';
 const _ = require('lodash');
-
-//2 spanish, 3 italian
 
 class App extends Component {
 
@@ -70,42 +69,12 @@ class App extends Component {
     this.setState({aboutFlag:!this.state.aboutFlag})
   }
 
-  renderTeam = () => {
-    let arr = [{name:'Charlie', url:'https://github.com/cboyce183'},{name:'Hannah', url:'https://github.com/redspanner'},{name:'Mike',url:'https://github.com/MPastorMeseguer'},{name:'Javier', url:'https://github.com/moranlemusj'},{name:'Jack',url:'https://github.com/vidocco'}]
-    arr = _.shuffle(arr);
-    return arr.map( el => {
-      return (
-        <a href={el.url} style={{textDecoration:'none'}}>
-          <div style={{padding:'5px',width:'60px',height:'60px',borderRadius:'35px', backgroundColor:'rgb(64,89,147)',display:'flex',justifyContent:'space-around',alignItems:'center'}}>
-            <p style={{fontFamily: 'Roboto', fontWeight: 'lighter', color: 'white'}}>{el.name}</p>
-          </div>
-        </a>
-      )
-    })
-  }
-
-  renderAboutWindow = () => {
-    if (this.state.aboutFlag) return (
-      <div className="about-container" onClick={this.handleAboutClick}>
-        <h1 className="about-title">Made by developers, useful for everyone.</h1>
-        <img src={require('./assets/icons8-multicultural-people-100.png')} style={{height:'80px'}}/>
-        <h3 className="about-text">uncode.js is a tool for learning how to code JavaScript in the most humanly friendly way possible: code, but in your own, comfortable native language.</h3>
-        <div style={{display: 'flex', flexFlow: 'row nowrap', alignItems: 'center', justifyContent: 'space-around'}}>
-          <p style={{fontFamily: 'Courier New, monospace;'}}>var foo = 10 + 5;</p>
-          <img src={require('./assets/icons8-right-100.png')} style={{height:'30px', padding: '20px'}}/>
-          <p>1 var variable foo is assigned to 10 + 5</p>
-        </div>
-        <h3 className="about-text">Simply start coding, or copy in some code from elsewhere, and read the steps the complier takes to run the code in terms anyone can understand. It is that simple!</h3>
-        <h3 className="about-text">The Team: </h3>
-        <div style={{display:'flex', flexFlow:'row nowrap',justifyContent:'space-around', paddingBottom:'5vh', width:'40%'}}>{this.renderTeam()}</div>
-      </div>
-    )
-  }
-
   render() {
     return (
       <div className="App">
-        {this.renderAboutWindow()}
+        <About
+          display={this.state.aboutFlag}
+          handleDisplay={this.handleAboutClick.bind(this)}/>
         <nav className="Header">
           <div className="MaxWidth">
             <LogoAnim />
@@ -141,6 +110,12 @@ class App extends Component {
                 <button className="">Upload</button>
               </ReactFileReader>
             </Dropzone> */}
+            <div className="TabSelector">
+              <div 
+                className="Tab Selected">editor</div>
+              <div 
+                className="Tab">upload</div>
+            </div>
             <div className="Form">
               <Text
                 func={this.handleTextChange.bind(this)}
